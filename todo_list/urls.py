fcf62849cbb0df12ca0a6119e7115c525cb3cfb4
Yevhenii Iovchu <persie.iovchu@gmail.com>
2023-07-26 12:56:29 +0300

    created view for tag to update and set url

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from todo_list.views import TagListView, TaskListView, TaskCreateView, TagCreateView, TaskUpdateView
+from todo_list.views import TagListView, TaskListView, TaskCreateView, TagCreateView, TaskUpdateView, TagUpdateView
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
@@ -8,6 +8,7 @@ urlpatterns = [
     path("<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update", TagUpdateView.as_view(), name="tag-update"),
 ]
 
 app_name = "todo_list"
