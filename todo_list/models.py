81a4d6b6ecbabddf16aeccdf43524a099f260204
Yevhenii Iovchu <persie.iovchu@gmail.com>
2023-07-26 10:26:07 +0300

    Created models for task and for tag

--- todo_list/models.py
+++ todo_list/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag (models.Model):
+    name = models.CharField(max_length=63)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=63)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_done = models.BooleanField()
+    tag = models.ManyToManyField(Tag, related_name="tasks")
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
