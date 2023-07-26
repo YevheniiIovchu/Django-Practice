73eae69b3aa240213eb7c692d74501eda1e02ab4
Yevhenii Iovchu <persie.iovchu@gmail.com>
2023-07-26 13:05:43 +0300

    created view for tag and task to delete, set url and made delete confirmation templates to them

--- todo_list/views.py
+++ todo_list/views.py
@@ -22,6 +22,12 @@ class TaskUpdateView(generic.UpdateView):
     success_url = reverse_lazy("todo_list:task-list")
 
 
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    template_name = "todo_list/task_confirm_delete.html"
+    success_url = reverse_lazy("todo_list:task-list")
+
+
 class TagListView(generic.ListView):
     model = Tag
 
@@ -35,4 +41,10 @@ class TagCreateView(generic.CreateView):
 class TagUpdateView(generic.UpdateView):
     model = Tag
     fields = "__all__"
+    success_url = reverse_lazy("todo_list:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    template_name = "todo_list/tag_confirm_delete.html"
     success_url = reverse_lazy("todo_list:tag-list")
