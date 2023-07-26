f6cc14743dfeefe4ad70d68d896513cd859c4172
Yevhenii Iovchu <persie.iovchu@gmail.com>
2023-07-26 15:46:44 +0300

    Some fixes in indentation

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views import generic, View
 
